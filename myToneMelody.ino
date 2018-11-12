98752b5fed037fa9f899a5de889aee5a3e11b8b3
DESKTOP-Q1D3SV2\Matthew <mromleski@gmail.com>
2018-11-11 22:13:33 -0600

    Modified the code to randomly generate notes & durations. Still uses the selection sort in place of something better. Also, delted the pitches.h file since it's no longer used.

--- myToneMelody.ino
+++ myToneMelody.ino
@@ -15,11 +15,11 @@
   http://www.arduino.cc/en/Tutorial/Tone
 */
 
-#include "pitches.h"
-
 // Function that sorts an array by finding the lowest value & moving it to the bottom.
 // It then increments so that it won't ever check the bottom value. It takes n^2 cycles to complete.
-void selectSort(int arrayIn[], int n) 
+// Repeats the operations done to the melody on the note duration, so that the
+// notes have the same length post-sorting as the did pre-sort.
+void selectSort(int arrayIn[], int arrayIn2[], int n) 
 { 
     int i, j, min_i; 
  
@@ -28,32 +28,50 @@ void selectSort(int arrayIn[], int n)
         min_i = i; 
         for (j = i+1; j < n; j++) 
           if (arrayIn[j] < arrayIn[min_i]) 
-            min_idx = j; 
+            min_i = j; 
   
-        swap(&arrayIn[min_i], &arrayIn[i]); 
+        swap(&arrayIn[min_i], &arrayIn[i]);
+        swap(&arrayIn2[min_i], &arrayIn2[i]);
+        
     } 
 } 
 
+// Generic swap function.
 void swap(int *x, int *y) 
 { 
     int temp = *x; 
     *x = *y; 
     *y = temp; 
-} 
-
-// notes in the melody:
-int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
-
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
-};
+}
 
 void setup() {
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  // Declare the variables.
+  int limit = random(5, 10);
+  int melody[limit];
+  int noteDurations[limit];
+
+  // Generate the random notes using the formula for the frequency of notes (2^((n-49)/12)*440).
+  for (int i = 0; i < limit; i++) {
+    int randnum = random(20, 70);
+    float exponent = (randnum-49)/12;
+    float freq = pow(2, exponent) * 440;
+    melody[i] = round(freq);
+  }
+  
+  // Generate random note durations. 
+  // 1 = 1000ms/2 = 500ms, 2 = 1000ms/4 = 250ms, 3 = 1000ms/8 = 125ms.
+  for (int i = 0; i < limit; i++) {
+    int randnum = random(1,3);
+    if(randnum == 1 || randnum == 2) {
+      noteDurations[i] = randnum * 2;
+    }
+    else {
+      noteDurations[i] = 8;
+    }
+  }
+
+  //Plays the song.
+  for (int thisNote = 0; thisNote < limit; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
@@ -67,10 +85,15 @@ void setup() {
     // stop the tone playing:
     noTone(8);
   }
-  
-  selectionSort(melody, 8);
-  
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+  // Sorts the song.
+  selectSort(melody, noteDurations, limit);
+
+  // Generate a pause between the 1st & 2nd playthrough.
+  delay(1500);
+
+  // Plays the song again.
+  for (int thisNote = 0; thisNote < limit; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
